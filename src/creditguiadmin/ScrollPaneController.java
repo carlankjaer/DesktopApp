4c1105b9d5066ee223235bd0af5680884e01ad3d
Paul <s175203@student.dtu.dk>
2018-06-07 16:29:35 +0200

    added gitignore etc

--- src/creditguiadmin/ScrollPaneController.java
+++ src/creditguiadmin/ScrollPaneController.java
@@ -1,18 +1,22 @@
 package creditguiadmin;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
+//import rest.DTO.Category;
+//import rest.DTO.Product;
+import rest.implementations.AuthenticationClientImpl;
+import rest.interfaces.AuthenticationClient;
 
 import java.net.URL;
 import java.util.*;
@@ -24,6 +28,31 @@ public class ScrollPaneController implements Initializable {
     public ScrollPane scrollPane;
     public Button categoriesButton;
     public TableView productTable;
+    public TableColumn productColumn;
+    public List<Product> productList = new ArrayList<>();
+    public Button payForChosenProducts;
+    public TextField createCustomerFirstName;
+    public TextField createCustomerLastName;
+    public TextField createCustomerAddress1;
+    public TextField createCustomerAddress1createCustomerAddress2;
+    public TextField createCustomerCity;
+    public TextField createCustomerZipCode;
+    public TextField createCustomerPhoneNumber;
+    public TextField createCustomerEmail;
+    public TextField createCustomerID;
+    public Button createCustomerButton;
+    public TextField createUserID;
+    public TextField createUserPhoneNumber;
+    public TextField createUserName;
+    public CheckBox adminRights;
+    public Button createUserButton;
+    public TextField refundCustomerID;
+    public TextField refundCustomerName;
+    public TextField currentRefundableBalance;
+    public Button refundLatestProduct;
+    public Button refundAllProducts;
+    public TextField deleteCustomerID;
+    public TextField deleteCustomerName;
 
     Product product1 = new Product(1, "kqly1");
     Product product2 = new Product(2, "kqly2");
@@ -87,8 +116,8 @@ public class ScrollPaneController implements Initializable {
             productButton.setOnAction(event -> scrollPane.setContent(new Button()));
 
             productButton.setStyle("-fx-font: 22 arial; -fx-base: #b6e7c9;");
-            productButton.setMaxSize(350,80);
-            productButton.setMinSize(350,80);
+            productButton.setMaxSize(300,80);
+            productButton.setMinSize(300,80);
 
             Button returnButton = new Button("back to 80's");
 
@@ -98,6 +127,7 @@ public class ScrollPaneController implements Initializable {
             returnButton.setMaxSize(150,30);
             returnButton.setMinSize(150,30);
             grid.add(returnButton, 0,0);
+
 
             productButton.setOnAction(event -> addToProductList(product));
 
@@ -112,13 +142,19 @@ public class ScrollPaneController implements Initializable {
 
     public void addToProductList(Product product) {
 
-        ArrayList<String> productList = new ArrayList<String>();
+        productList.add(product);
 
-        productList.add(product.getName());
+        ObservableList<Product> obsProductList = FXCollections.observableArrayList(productList);
 
-        
+        productColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("name"));
 
-        System.out.println("" + productList);
+        productTable.setItems(obsProductList);
+
+    }
+
+    public void searchProducts() {
+
+
 
     }
 
--- src/mainlogin/Controller.java
+++ src/mainlogin/Controller.java
@@ -5,38 +5,74 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
+
+    public static String[] userAdmin = {"1", "1"};
+    public static String[] userUser = {"2", "2"};
+    public TextField userID;
+    public PasswordField passwordID;
+
     public void loginFunction (ActionEvent event)
     {
+        if (userID.getText().equals(userAdmin[0]) && passwordID.getText().equals(userAdmin[1])) {
+            Parent root;
+            try
+            {
 
-        Parent root;
-        try
-        {
+                root = FXMLLoader.load(getClass().getClassLoader().getResource("creditguiadmin/creditGUI.fxml"));
+                Stage home = new Stage();
+                home.setTitle("Credit GUI");
+                home.setScene(new Scene(root, 1031.0, 800.0));
+                home.setMinHeight(800);
+                home.setMaxHeight(800);
+                home.setMinWidth(1031);
+                home.setMaxWidth(1031);
+                home.show();
 
-            root = FXMLLoader.load(getClass().getClassLoader().getResource("creditguiadmin/creditGUI.fxml"));
-            Stage home = new Stage();
-            home.setTitle("Credit GUI");
-            home.setScene(new Scene(root, 1031.0, 800.0));
-            home.setMinHeight(800);
-            home.setMaxHeight(800);
-            home.setMinWidth(1031);
-            home.setMaxWidth(1031);
-            home.show();
+                ((Node)(event.getSource())).getScene().getWindow().hide();
 
-            ((Node)(event.getSource())).getScene().getWindow().hide();
+            }
 
+            catch (IOException e)
+            {
+
+                System.out.println(e);
+
+            }
+
+        } else if(userID.getText().equals(userUser[0]) && passwordID.getText().equals(userUser[1])){
+            Parent root;
+            try
+            {
+
+                root = FXMLLoader.load(getClass().getClassLoader().getResource("creditguiuser/creditGUIuser.fxml"));
+                Stage home = new Stage();
+                home.setTitle("Credit GUI");
+                home.setScene(new Scene(root, 1031.0, 800.0));
+                home.setMinHeight(800);
+                home.setMaxHeight(800);
+                home.setMinWidth(1031);
+                home.setMaxWidth(1031);
+                home.show();
+
+                ((Node)(event.getSource())).getScene().getWindow().hide();
+
+            }
+
+            catch (IOException e)
+            {
+
+                System.out.println(e);
+
+            }
         }
-
-        catch (IOException e)
-        {
-
-            System.out.println(e);
-
-        }
-
+        else
+            System.out.println("Unauthorized access");
     }
 }
